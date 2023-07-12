6d7fc695620c4fc6e02871a8ba493a0bdfe777d9
RMS83 <SRoman2005@mail.ru>
2023-07-12 20:02:50 +0300

    add scheme_db and user_model

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,34 @@
 #!/bin/sh
+#sleep 10
 
 python manage.py makemigrations
+python manage.py makemigrations order_app
 python manage.py migrate
 
 #For Redis
 #python manage.py createcachetable
 
+# Option 1. executing a file to create a superuser
 if [ "$DJANGO_SUPERUSER_USERNAME" ] && [ "$POSTGRES_DB" ]
   then
     echo "creating a superuser - ${DJANGO_SUPERUSER_USERNAME}."
     python manage.py createsuperuser \
         --noinput \
-        --username $DJANGO_SUPERUSER_USERNAME \
-        --email $DJANGO_SUPERUSER_EMAIL
+        --email $DJANGO_SUPERUSER_EMAIL \
+        --first_name $DJANGO_SUPERUSER_NAME \
+        --last_name $DJANGO_SUPERUSER_SURNAME \
+
     echo "CREATED SUPERUSER"
 fi
 
-
-## Option 2. executing a file to create a superuser
-#if [ "$DJANGO_SUPERUSER_USERNAME" ]
+#
+# Option 2. executing a file to create a superuser
+#if [ "$DJANGO_SUPERUSER_EMAIL" ] && [ "$POSTGRES_DB" ]
 #  then
-#    echo "creating a superuser - ${DJANGO_SUPERUSER_USERNAME}."
+#    echo "creating a superuser - ${DJANGO_SUPERUSER_EMAIL}."
 #    echo "from django.contrib.auth import get_user_model" > administry.py
 #    echo "User = get_user_model()" >> administry.py
-#    echo "User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" >> administry.py
+#    echo "User.objects.create_superuser('$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_NAME', '$DJANGO_SUPERUSER_SURNAME', '$DJANGO_SUPERUSER_PASSWORD')" >> administry.py
 #    python3 manage.py shell < administry.py
 #    echo "CREATED SUPERUSER"
 #    rm administry.py
